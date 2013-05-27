306cab3e19250668669dd7447df4ce7fb14bcdac
Ivo Vasilev <ivo@bulklabs.com>
2013-05-27 17:20:05 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanderways::Application.config.secret_token = 'ca2e2e5a831c55be407c788577bce57b90f2395ea3b705e808528eef8f5aae8796750c324b86bc004fdaa90ed8fa67d9ca47ae8d80546d3641d2bb6cb1c2cd89'
